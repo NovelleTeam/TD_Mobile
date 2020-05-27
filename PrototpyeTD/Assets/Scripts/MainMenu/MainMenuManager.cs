9d6e77908b2efeac172a49572519bc674a29e7ef
nikinov <novellenicholas@gmail.com>
2020-05-27 18:10:53 +0200

    simple rocket

---
+++ PrototpyeTD/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void goToPlayScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneBuildIndex: currentScene + 1);
+    }
+}
---
+++ PrototpyeTD/Assets/Scripts/PlayScene/RocketManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketManager : MonoBehaviour
+{
+    public GameObject rocket;
+
+    public void spawnRocket()
+    {
+
+    }
+}
