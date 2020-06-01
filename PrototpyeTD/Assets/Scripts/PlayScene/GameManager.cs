31e39fe0b1228c547fca8189bbb3d4e1db4cfcaa
nikinov <novellenicholas@gmail.com>
2020-06-01 16:57:18 +0200

    little fix

---
+++ PrototpyeTD/Assets/Scripts/PlayScene/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup BalckPanel;
+
+    private void Start()
+    {
+        LeanTween.alphaCanvas(BalckPanel, 0, 1);
+    }
+}
